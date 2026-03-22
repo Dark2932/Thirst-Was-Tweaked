93a1c5c31c17dffa6eeb044f76052887e94db9f9
Dark2932 <lqy070821@163.com>
2026-03-22 20:11:57 +0800

    fix crash on server

--- src/main/java/com/dark2932/thirst_was_tweaked/mixin/thirst/accessor/HUDOverlayHandlerAccessor.java
+++ src/main/java/com/dark2932/thirst_was_tweaked/mixin/thirst/accessor/HUDOverlayHandlerAccessor.java
@@ -1,12 +1,15 @@
 package com.dark2932.thirst_was_tweaked.mixin.thirst.accessor;
 
 import dev.ghen.thirst.foundation.gui.appleskin.HUDOverlayHandler;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 /**
  * @author Dark2932
  */
+@OnlyIn(Dist.CLIENT)
 @Mixin(value = HUDOverlayHandler.class, remap = false)
 public interface HUDOverlayHandlerAccessor {
 
